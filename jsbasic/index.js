55a385c70f2d0254e703fbaffcfc93e2ee2dcc06
yamunaacharya <yamunaacharya2060@gmail.com>
2025-12-09 12:17:00 +0545

    basics of JS

---
+++ jsbasic/index.js
@@ -0,0 +1,36 @@
+// // let a=5;
+// // let b=4;
+// // let c=a+b;
+
+// // console.log(c);
+
+// //object format
+// let person = {
+//     name: "Yamuna",
+//     age: 21,
+//     class: 15
+// }
+
+// console.log(person.name)
+
+// //array format
+// // let arrayPerson = ["yamuna",21,15]
+// // console.log(arrayPerson[0])
+
+// let nameofanimals = ["tiger","lion","Leopard"]
+// console.log(nameofanimals[2])
+
+// for(let i=0; i<nameofanimals.length; i++){
+//     console.log(nameofanimals[i])
+// }
+
+
+//concatination
+
+let a=50;
+console.log("I am"+a + "ushhhhhhhhhhhaw + a" +a)
+
+//templet literals
+
+let name = "Ram";
+console.log(`my name is ${name} name`)
